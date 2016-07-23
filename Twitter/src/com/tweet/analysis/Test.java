c59f75f9c3d12dbdf13cf02419b1fb8309e7be84
Kuldeep <kuldeep03.nitb@gmail.com>
2016-07-23 17:26:08 +0530

    minor refactoring

--- Twitter/src/com/tweet/analysis/Test.java
+++ Twitter/src/com/tweet/analysis/Test.java
@@ -21,46 +21,40 @@ public final class Test {
 	private static int numOfPages = 10;
 
 	public static void main(String[] args) throws Exception {
-		
-		if(args.length != 2){
+
+		if (args.length != 2) {
 			throw new Exception("Correct Usage..UserId1 UserId2");
 		}
 
 		Twitter twitter = new TwitterFactory().getInstance();
-
-		int i =1;
-		int j=1;
+		int i = 0;
 		int count = 0;
-		for(int page =1; page <= numOfPages ; page++){
+		for (int page = 1; page <= numOfPages; page++) {
 			List<Status> allStatus = null;
 			try {
-				allStatus = twitter.getUserTimeline(args[0],new Paging(page, 25));
-			} 
-			catch (TwitterException e) {
+				allStatus = twitter.getUserTimeline(args[0], new Paging(page, 25));
+			} catch (TwitterException e) {
 				log.error("Error occured while fetching the tweets of a user..");
 				e.printStackTrace();
 			}
-			if(allStatus != null){
-				for(Status status : allStatus){
-					count = count +1;
-					if(status.getText().contains(args[1])){
-						i++ ;
-					}					
+			if (allStatus != null) {
+				for (Status status : allStatus) {
+					count = count + 1;
+					if (status.getText().contains(args[1])) {
+						i++;
+						System.out.println(status.getText());
+					}
 
 				}
 
-				
-			}
-			else{
+			} else {
 				log.error("Somthing bad happened ..check the logs for more details..");
 			}
 
-			
 		}
-		
-		System.out.println("No. of tweets done by " + args[0] +  ":"  + count);
+
+		System.out.println("No. of tweets done by " + args[0] + ":" + count);
 		System.out.println("No. of tweets in which he mentioned " + args[1] + ":" + i);
-		
 
 	}
 }
