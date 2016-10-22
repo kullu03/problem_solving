4e069e738a46d6468a3760b2b95c0c12ea03b6a4
Kuldeep <kuldeep03.nitb@gmail.com>
2016-10-22 20:30:37 +0530

    spell counter

--- gfg/src/com/intw/practice/string/SpellCounter.java
+++ gfg/src/com/intw/practice/string/SpellCounter.java
@@ -6,13 +6,13 @@ import java.util.Map;
 public class SpellCounter {
 
 	public static void main(String[] args) {
-		
+
 		System.out.println(getSpellCount2("100"));
 		System.out.println(getSpellCount2("11112"));
 		System.out.println(getSpellCount2("8884441100"));
 		System.out.println(getSpellCount2("12345"));
 		System.out.println(getSpellCount2("11111"));
-		
+
 	}
 
 	/*
@@ -21,9 +21,10 @@ public class SpellCounter {
 	 * 
 	 */
 	private static int getSpellCount(String input) {
-		if(input == null || input.isEmpty()){
+		if (input == null || input.isEmpty()) {
 			return 0;
 		}
+
 		Map<Character, Integer> map = new HashMap<Character, Integer>();
 
 		for (Character c : input.toCharArray()) {
@@ -42,27 +43,28 @@ public class SpellCounter {
 		}
 		return (int) Math.pow(2, exp);
 	}
-	
+
 	/*
 	 * This is assumed that input String contains only numbers.
 	 * 
 	 * 
 	 */
-	private static int getSpellCount2(String input){
+	private static int getSpellCount2(String input) {
 
-		if(input == null || input.isEmpty()) return 0;
-	
-		int count  = 1;
-		for(int i = 0; i < input.length() -1; i++){
+		if (input == null || input.isEmpty())
+			return 0;
+
+		int count = 1;
+		for (int i = 0; i < input.length() - 1; i++) {
 
 			while (i < input.length() - 1 && input.charAt(i) == input.charAt(i + 1)) {
-				count = count +1;
+				count = count + 1;
 				i = i + 1;
 			}
 		}
-		
-		return (int) Math.pow(2, count-1);
-		
+
+		return (int) Math.pow(2, count - 1);
+
 	}
 
 }
